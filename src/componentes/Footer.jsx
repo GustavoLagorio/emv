54c64ce2eef905d4f1cb4318fb7f1fee74e54a97
Gustavo Lagorio <lagoriog3287@gmail.com>
2024-05-01 12:57:09 -0300

    logo autor2

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Autor from "../assets/autor_logo.jpeg";
+import Autor1 from "../assets/autor1_logo.jpeg";
+import Autor2 from "../assets/autor2_logo.jpg";
 import { Link } from "react-router-dom";
 import { SocialLinks } from "./SocialLinks";
 
@@ -13,7 +14,18 @@ export const Footer = () => {
         <section className="terms">
           <span className="developer">
             <span className="autor">
-              <img className="autor-logo" src={Autor} alt="Helios Web Desing" />
+              <a href="">
+                <img
+                  className="autor-logo1"
+                  src={Autor1}
+                  alt="Helios Web Desing"
+                />
+              </a>
+            </span>
+            <span className="autor2">
+              <a href="">
+                <img className="autor-logo2" src={Autor2} alt="" />
+              </a>
             </span>
             <span className="copyright">
               <p>&copy; {currentYear}</p>
